4751272c65e52ad779ee1332d2b0edeb54fd2cb7
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-08-20 19:01:09 +0200

    chore: Refactor Config class to use a configurable file path for loading and changing settings

--- src/helper/config.py
+++ src/helper/config.py
@@ -5,6 +5,7 @@ from yaml import SafeLoader
 
 class Config:
     _instance = None
+    CONFIG_FILE_PATH = "config.yaml"
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
@@ -19,40 +20,40 @@ class Config:
         self._initialized = True
 
     def load_config(self):
-        with open("config.yaml", "r") as file:
-            self.config = yaml.load(file, Loader=SafeLoader)
+        with open(self.CONFIG_FILE_PATH, "r") as file:
+            self.settings = yaml.load(file, Loader=SafeLoader)
 
         self.rainbow_line_gif: str = "https://i.imgur.com/mnydyND.gif"
-        self.app_logo: str = self.config.get("app_logo", "")
-        self.app_url: str = self.config.get("app_url", "")
-        self.app_name: str = self.config.get("app_name", "")
+        self.app_logo: str = self.settings.get("app_logo", "")
+        self.app_url: str = self.settings.get("app_url", "")
+        self.app_name: str = self.settings.get("app_name", "")
         self.app_name_branded: str = f"{self.app_name} • {self.app_url}"
-        self.app_version: str = self.config.get("app_version", "")
-        self.log_file = self.config.get("log_file", "")
+        self.app_version: str = self.settings.get("app_version", "")
+        self.log_file = self.settings.get("log_file", "")
 
-        self.bot_prefix: str = self.config.get("bot_prefix", "!")
-        self.bot_token: str = self.config.get("bot_token", "")
-        self.panel_channel_id: int = int(self.config.get("panel_channel_id", 0))
-        self.panel_message_id: int = int(self.config.get("panel_message_id", 0))
-        self.logs_channel: int = int(self.config.get("logs_channel", 0))
-        self.dev_guild_id: discord.Object = discord.Object(int(self.config.get("dev_guild_id", 0)))
+        self.bot_prefix: str = self.settings.get("bot_prefix", "!")
+        self.bot_token: str = self.settings.get("bot_token", "")
+        self.panel_channel_id: int = int(self.settings.get("panel_channel_id", 0))
+        self.panel_message_id: int = int(self.settings.get("panel_message_id", 0))
+        self.logs_channel: int = int(self.settings.get("logs_channel", 0))
+        self.dev_guild_id: discord.Object = discord.Object(int(self.settings.get("dev_guild_id", 0)))
 
-        self.azuracast_station_url: str = self.config.get("azuracast_station_url", "")
-        self.azuracast_station_name: str = self.config.get("azuracast_station_name", "")
+        self.azuracast_station_url: str = self.settings.get("azuracast_station_url", "")
+        self.azuracast_station_name: str = self.settings.get("azuracast_station_name", "")
 
-        self.azuracast_api_url: str = self.config.get("azuracast_api_url", "")
-        self.azuracast_api_key: str = self.config.get("azuracast_api_key", "")
+        self.azuracast_api_url: str = self.settings.get("azuracast_api_url", "")
+        self.azuracast_api_key: str = self.settings.get("azuracast_api_key", "")
 
     def change_value(self, key, value):
         try:
-            with open("config.yaml", "r") as file:
+            with open(self.CONFIG_FILE_PATH, "r") as file:
                 config = yaml.load(file, Loader=SafeLoader)
             config[key] = value
-            with open("config.yaml", "w") as file:
+            with open(self.CONFIG_FILE_PATH, "w") as file:
                 yaml.dump(config, file)
-            logger.info(f"Changed value in config.yaml: {key} -> {value}, the file was rewritten.")
+            logger.info(f"Changed value in {self.CONFIG_FILE_PATH}: {key} -> {value}, the file was rewritten.")
             self.load_config()
             return True
         except Exception as e:
-            logger.critical(f"Failed to change value in config.yaml: {e}")
+            logger.critical(f"Failed to change value in {self.CONFIG_FILE_PATH}: {e}")
             return False
